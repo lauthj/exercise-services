f5300c404131025c9b81e7a9c6742519911ab8f1
joseph.lauth <joseph.lauth@CPX-5UCKMCA0IMV.dir.svc.accenture.com>
2019-09-26 15:26:29 -0500

    updated

--- src/main/java/com/pluralsight/ActivityResource.java
+++ src/main/java/com/pluralsight/ActivityResource.java
@@ -1,6 +1,5 @@
 package com.pluralsight;
 
-import java.util.Collection;
 import java.util.List;
 
 import javax.ws.rs.GET;
@@ -9,7 +8,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.pluralsight.model.Activity;
-import com.pluralsight.repository.ActivityRepository;
 import com.pluralsight.repository.ActivityRepositoryStub;
 
 @Path("activities")
